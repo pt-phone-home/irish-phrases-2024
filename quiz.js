43fd3c1754ca22cd187c09d79eacebe5dbad2c32
Peter Tiernan <ptiernan@gmail.com>
2024-12-27 14:52:25 +0000

    initial commit for quiz feature

---
+++ quiz.js
@@ -0,0 +1,44 @@
+const startQuiz = document.getElementById("startQuiz");
+const quizContainer = document.getElementById("quizContainer");
+let currentQuiz = [];
+let currentIndex = 0;
+let score = 0;
+
+async function getQuizPhrases() {
+  const response = await fetch("./Phrases_2024_utf8.json");
+  const phrases = await response.json();
+
+  const a = Math.floor(Math.random() * phrases.length) + 1;
+  const b = Math.floor(Math.random() * phrases.length) + 1;
+  const c = Math.floor(Math.random() * phrases.length) + 1;
+  const d = Math.floor(Math.random() * phrases.length) + 1;
+  const e = Math.floor(Math.random() * phrases.length) + 1;
+  const f = Math.floor(Math.random() * phrases.length) + 1;
+  const g = Math.floor(Math.random() * phrases.length) + 1;
+  const h = Math.floor(Math.random() * phrases.length) + 1;
+  const i = Math.floor(Math.random() * phrases.length) + 1;
+  const j = Math.floor(Math.random() * phrases.length) + 1;
+
+  currentQuiz = [
+    phrases[a],
+    phrases[b],
+    phrases[c],
+    phrases[d],
+    phrases[e],
+    phrases[f],
+    phrases[g],
+    phrases[h],
+    phrases[i],
+    phrases[j],
+  ];
+  console.log(currentQuiz);
+  currentIndex = 0;
+  score = 0;
+  quizContainer.style.display = "block";
+  displayQuestions();
+}
+
+function displayQuestions() {}
+
+// Event listeners
+startQuiz.addEventListener("click", getQuizPhrases);
